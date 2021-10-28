573a6048063a4c9713e70abf24e2b6189a542bea
hanscj1 <cjones@cloudm2.com>
2021-10-28 15:51:14 -0400

    Updated lsrepo naming

--- ansible/playbooks/roles/repository/files/client/Debian/add-epirepo-client.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -eu
-
-REPOSITORY_URL=$1
-
-echo "deb [trusted=yes] $REPOSITORY_URL/packages ./" > /etc/apt/sources.list.d/lsrepo.list
-
-apt update
--- ansible/playbooks/roles/repository/files/client/Debian/disable-epirepo-client.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -eu
-
-rm -f /etc/apt/sources.list.d/lsrepo.list
-apt-get clean
-apt update

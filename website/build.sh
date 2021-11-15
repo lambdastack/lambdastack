8b2c74df420005b5580913c233ae7537252b555c
hanscj1 <cjones@cloudm2.com>
2021-11-15 09:29:45 -0500

    Updated S3 bucket for website

--- website/build.sh
+++ website/build.sh
@@ -4,4 +4,4 @@
 
 rm -rf public/
 HUGO_ENV="production" hugo --gc || exit 1
-s3deploy -source=public/ -region=us-east-1 -bucket=lambdastackio
+s3deploy -source=public/ -region=us-east-1 -bucket=www.lambdastackio.com -acl='public-read'
